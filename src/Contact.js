f52583c334bdebb7d72900039cefe5b747d240c1
wchap23 <wchap23@wgu.edu>
2024-11-19 23:02:13 -0500

    Restoring files and pushing to gh-pages

--- src/Contact.js
+++ src/Contact.js
@@ -2,14 +2,36 @@ import './Contact.css';
 
 const Contact = () => {
     return (
-<div class="feedback-card">
-  <div class="feedback-header" id='contact'>
-    Contact Me!
+  <div id="contact" class="card2">
+  <div class="card-content2">
+    <div class="profile-section2">
+      <img src={`${process.env.PUBLIC_URL}/assets/GitHub.png`} alt="GitHub" class="profile-image2" />
+      <a href="https://github.com/PkTaz" class="profile-link2" target="_blank">
+        @PkTaz
+      </a>
+      <div className="contact-info">
+      <p className="phone-number">(517) 438-6578</p>
+      </div>
+      <div className="bar2">
+        <div className="emptybar2"></div>
+        <div className="filledbar2"></div>
+      </div>
+    </div>
+    <div class="profile-section2">
+      <img src={`${process.env.PUBLIC_URL}/assets/LinkedIn.png`} alt="LinkedIn" class="profile-image2" />
+      <a href="https://www.linkedin.com/in/william-chapman-80944b254/" class="profile-link2" target="_blank">
+        @William-Chapman
+      </a>
+    </div>
+    <div class="profile-section2">
+      <img src={`${process.env.PUBLIC_URL}/assets/Email.png`} alt="Email Me" class="profile-image2" />
+      <a href="mailto:chapmanw811@gmail.com" class="profile-link2" target="_blank">
+      chapmanw811@gmail.com
+      </a>
+    </div>
   </div>
-  <form class="feedback-body">
-    <input type="email" class="feedback-body__email" placeholder="Email" /><textarea type="text" class="feedback-body__message" placeholder="Message"></textarea><button class="feedback-body__submit">SEND</button>
-  </form>
 </div>
+
     );
 };
 
